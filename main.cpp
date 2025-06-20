d05c2a71b75a6399e6401d28e6e83ec4f39ee438
kuldeep1203 <kuldeeppawar427@gmail.com>
2025-06-20 17:25:29 +0400

    test run

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <gtkmm.h>
+#include<iostream>
+using namespace std;
+class MyWindow : public Gtk::Window
+{
+public:
+  MyWindow();
+};
+
+MyWindow::MyWindow()
+{
+  set_title("Basic application");
+  set_default_size(200, 200);
+}
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("org.gtkmm.examples.base");
+
+  
+  return app->make_window_and_run<MyWindow>(argc, argv);
+}
